fa216026eff73bf1a5b24077530198cb5d615e8f
ADMIN <honaz140@gmaill.com>
2017-04-13 08:37:44 +0700

    update code in onCreate() method of PlayerActivity.java

--- app/src/main/java/com/example/admin/musicplayer/view/PlayerActivity.java
+++ app/src/main/java/com/example/admin/musicplayer/view/PlayerActivity.java
@@ -1,5 +1,6 @@
 package com.example.admin.musicplayer.view;
 
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.example.admin.musicplayer.R;
+import com.example.admin.musicplayer.controller.SongManager;
 import com.example.admin.musicplayer.model.SongModel;
 
 import java.util.ArrayList;
@@ -18,7 +20,7 @@ import java.util.ArrayList;
 public class PlayerActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener,
         MediaPlayer.OnCompletionListener{
     public static final int SELECT_SONG_REQUEST = 0;
-    private Toolbar toolbar;
+    private Toolbar toolBar;
     private SongModel playingSong;
     public static ArrayList<SongModel> arrSongs = new ArrayList<>();
     private int seekForwardTime = 5000; // 5000 milliseconds
@@ -30,6 +32,7 @@ public class PlayerActivity extends AppCompatActivity implements View.OnClickLis
     private Handler mHandler = new Handler();
     private ImageView playButton;
     private ImageView forwardButton;
+    private ImageView backwardButton;
     private ImageView nextButton;
     private ImageView previousButton;
     private SeekBar songProgressBar;
@@ -41,6 +44,44 @@ public class PlayerActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_player);
 
         //get action bar
+        toolBar = (Toolbar) findViewById(R.id.title_tb);
+        toolBar.setTitle("Mp3 Player");
+        toolBar.setTitleTextColor(Color.BLACK);
+        toolBar.setNavigationIcon(R.drawable.icon_app);
+        setSupportActionBar(toolBar);
+        //get all song from sd card
+
+        SongManager songManager = new SongManager();
+        arrSongs = songManager.getPlayList();
+
+        //Media player is used to play music
+        mediaPlayer = new MediaPlayer();
+
+        // map views to player_activity_xml
+        playButton=(ImageView) findViewById(R.id.btn_play);
+        nextButton=(ImageView) findViewById(R.id.btn_next_song);
+        previousButton=(ImageView) findViewById(R.id.btn_back_song);
+        forwardButton=(ImageView) findViewById(R.id.btn_forward);
+        backwardButton=(ImageView) findViewById(R.id.btn_rewind);
+        songProgressBar=(SeekBar) findViewById(R.id.progress_seekbar);
+        currentDurationTextView=(TextView) findViewById(R.id.tv_current_time);
+        totalDurationTextView=(TextView) findViewById(R.id.tv_total_time);
+        // call event listener of each view
+        playButton.setOnClickListener(this);
+        forwardButton.setOnClickListener(this);
+        backwardButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+        previousButton.setOnClickListener(this);
+        
+        songProgressBar.setOnSeekBarChangeListener(this);// bat su kien de user biet bai hat dang tua den dau
+        mediaPlayer.setOnCompletionListener(this);// su kien de biet khi nao mediaplayer choi xong 1 bai nhac
+        
+        playSong(currentSongIndex);
+
+
+    }
+
+    private void playSong(int currentSongIndex) {
     }
 
     @Override
